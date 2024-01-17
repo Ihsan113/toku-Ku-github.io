6cdf89924099ad15437d3797ff1b89126f45873a
Ihsan113 <122337042+Ihsan113@users.noreply.github.com>
2024-01-17 18:17:56 +0700

    Add files via upload

---
+++ Admin/firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDPp8I8Xp-qZg65GxEX8_ETxvrZCDG8454",
+  authDomain: "bisnis-532f5.firebaseapp.com",
+  projectId: "bisnis-532f5",
+  storageBucket: "bisnis-532f5.appspot.com",
+  messagingSenderId: "351800625123",
+  appId: "1:351800625123:web:8abff984b15be7a00bc2d6",
+  measurementId: "G-TJJBQ9TF7S"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const redirectUrl = 'halaman-lain.html'; // Langsung mengarahkan ke halaman lain window.location.href = redirectUrl;
